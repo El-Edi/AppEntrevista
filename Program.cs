72964ce57a1646b622fa4dcbf95abe09308e5012
El-Edi <104475665+El-Edi@users.noreply.github.com>
2023-10-06 09:51:37 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using AppEntrevista.Datos;
+using AppEntrevista.Negocio;
+namespace EntrevistaApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Bienvenido al AppEntrevista de Escalada de Risco");
+                Console.WriteLine("-----------------------------------------");
+
+                Console.Write("Escribe el Mínimo de calorías: ");
+                int minCalorias = int.Parse(Console.ReadLine());
+
+                Console.Write("Escribe Peso máximo: ");
+                int pesoMaximo = int.Parse(Console.ReadLine());
+
+                if (minCalorias<0 || pesoMaximo<0)
+                {
+                    throw new ArgumentException("El valor ingresado no puede ser negativo");
+                }
+
+                EscaladaRisco escaladaRiscoService = new EscaladaRisco();
+                RepositorioElementos repositorioElementos = new RepositorioElementos();
+
+                List<Elemento> elementos = repositorioElementos.ObtenerElementos();
+                List<Elemento> elementosOptimos = escaladaRiscoService.EncontrarCombinacionOptima(elementos, minCalorias, pesoMaximo);
+
+                escaladaRiscoService.MostrarElementosOptimos(elementosOptimos);
+
+                Console.WriteLine("\nPresione cualquier tecla para salir...");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Debe ingresar números enteros válidos.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se ha producido una excepción al momento de ingresar los valores del peso máximo y calorias minimas: {ex.Message}");
+            }
+            
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# App Entrevista
+Una app básica por consola que permite encontrar la mejor combinación de elementos para escalar un risco, con un listado de elementos anteriormente dados, basandose en el peso y las calorías que necesite 
+el escalador.
+
+## Uso
+1. Ejecutar la solución la cual esta dentro de la carpeta AppEntrevista.Presentación con el nombre AppEntrevista.Presentación.sln.
+2. Ingresa la cantidad de peso máxima que puede cargar y el número minimo de calorías.
+3. Presiona enter para ver el listado de elementos optimos para llevar a la escalada.
+4. El resultado se mostrará en la consola.
+
+## Historial de Cambios
+
+### Versión 1.0.0 (Fecha de Lanzamiento: 05/10/2023)
+
+- Funcionalidad básica de la aplicación con adaptabilidad a crecimiento.
+- Interfaz de usuario por consola.
